25b9b49ad40c1c1f0eac7719eef8f4b1f34c2f2f
LucasCorrea <eslucascorrea@gmail.com>
2024-08-07 15:56:52 -0300

    Initial Project and configs
    1# - Added template react
    2# - Config global styled
    3# - Config theme styled
    4# - Added initial architecture front

---
+++ src/components/page/App.tsx
@@ -0,0 +1,17 @@
+
+import { StyledButton } from './App.style'
+
+
+function App() {
+  
+  return (
+    <>
+
+      <h1>Front d</h1>
+      <StyledButton> hi</StyledButton>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/styles/themes/primary.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'primary',
+
+    colors: {
+        primary: 'blue',
+        secundary: '#F5F5F5',
+        background: 'red',
+    }
+}
